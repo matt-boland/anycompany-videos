6c858f4aefcca70680f72b921aa6beae52cc85ef
Boland <mtboland@3c063048004d.ant.amazon.com>
2022-09-01 15:52:21 -0500

    added video player

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 import { Flex } from '@aws-amplify/ui-react';
-import { Ampligram, NavBar, SideBar } from './ui-components';
+import { Ampligram, NavBar, SideBar, AmpligramCollection} from './ui-components';
+import Videojs from './video.js'
 
 
 function App() {
+
+  const videoJsOptions = {
+    autoplay: true,
+    controls: true,
+    sources: [{
+      src: 'https://d69f1z2duoru5.cloudfront.net/ce7bd76a-a11a-4b5b-9311-a2bf314f6f7e/hls/SampleVideo_1280x720_30mb.m3u8',
+      type: 'application/x-mpegURL'
+    }]
+  };
+
   return (
     <div className="App">
       <NavBar overrides={{"Logo29767075": {children: "Any Company"}}} />
       <Flex direction="row">
         <SideBar />
-        <Ampligram />
+        <Videojs
+        { ...videoJsOptions } />
       </Flex>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -11,11 +11,11 @@ Amplify.configure(awsconfig);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
+  
     <AmplifyProvider>
       <App />
     </AmplifyProvider>
-  </React.StrictMode>
+  
 );
 
 // If you want to start measuring performance in your app, pass a function
